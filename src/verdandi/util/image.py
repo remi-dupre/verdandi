4407ce10ffa4adc18a9330128d37ff4ae531231c
Rémi Dupré <remi@dupre.io>
2025-12-18 18:12:43 +0100

    save image as an actual 2-bits PNG

--- src/verdandi/util/image.py
+++ src/verdandi/util/image.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 def image_to_bytes(img: Image.Image) -> bytes:
     buffer = BytesIO()
-    img.save(buffer, "png")
+    img = img.quantize(colors=4)
+    img.save(buffer, "png", bits=2)
     buffer.seek(0)
     return buffer.read()
