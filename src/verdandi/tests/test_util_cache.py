0a419356fbb035eff9d761ed2ebb51b910586d1b
Rémi Dupré <remi@dupre.io>
2026-01-27 22:33:52 +0100

    fix: cache was deadlocking after an exception was raised

---
+++ src/verdandi/tests/test_util_cache.py
@@ -0,0 +1,101 @@
+import asyncio
+from datetime import datetime, date, time, timedelta, UTC
+
+import pytest
+import time_machine
+
+from verdandi.util.cache import async_time_cache
+
+
+async def test_cache_over_time():
+    @async_time_cache(timedelta(hours=1))
+    async def get_time() -> datetime:
+        await asyncio.sleep(0.01)
+        return datetime.now(UTC).replace(second=0, microsecond=0)
+
+    d = date(2025, 7, 1)
+    t1 = datetime.combine(d, time(12, 0), tzinfo=UTC)
+    t2 = datetime.combine(d, time(12, 20), tzinfo=UTC)
+    t3 = datetime.combine(d, time(13, 1), tzinfo=UTC)
+
+    with time_machine.travel(t1):
+        assert await get_time() == t1
+
+    with time_machine.travel(t2):
+        assert await get_time() == t1
+
+    with time_machine.travel(t3):
+        assert await get_time() == t3
+
+
+async def test_cache_after_crash():
+    class FirstCrash(Exception): ...
+
+    @async_time_cache(timedelta(hours=1))
+    async def crash_once() -> int:
+        num_calls = getattr(crash_once, "num_calls", 0) + 1
+        setattr(crash_once, "num_calls", num_calls)
+        await asyncio.sleep(0.01)
+
+        if num_calls == 1:
+            raise FirstCrash("It will crash once")
+
+        return num_calls
+
+    d = date(2025, 7, 1)
+    t1 = datetime.combine(d, time(12, 0), tzinfo=UTC)
+    t2 = datetime.combine(d, time(12, 20), tzinfo=UTC)
+    t3 = datetime.combine(d, time(13, 1), tzinfo=UTC)
+    t4 = datetime.combine(d, time(13, 21), tzinfo=UTC)
+
+    with time_machine.travel(t1):
+        with pytest.raises(FirstCrash):
+            await crash_once()
+
+    # First successful run, start the timer
+    with time_machine.travel(t2):
+        assert await crash_once() == 2
+
+    # The timer is not yet expired (because it starts at t2, not t1)
+    with time_machine.travel(t3):
+        assert await crash_once() == 2
+
+    # The timer expired
+    with time_machine.travel(t4):
+        assert await crash_once() == 3
+
+
+async def test_concurrent_crash():
+    class FirstCrash(Exception): ...
+
+    @async_time_cache(timedelta(hours=1))
+    async def crash_once() -> int:
+        num_calls = getattr(crash_once, "num_calls", 0) + 1
+        setattr(crash_once, "num_calls", num_calls)
+        await asyncio.sleep(0.01)
+
+        if num_calls == 1:
+            raise FirstCrash("It will crash once")
+
+        return num_calls
+
+    d = date(2025, 7, 1)
+    t1 = datetime.combine(d, time(12, 0), tzinfo=UTC)
+    t2 = datetime.combine(d, time(12, 20), tzinfo=UTC)
+    t3 = datetime.combine(d, time(13, 1), tzinfo=UTC)
+
+    with time_machine.travel(t1):
+        task_1 = asyncio.create_task(crash_once())
+        await asyncio.sleep(0.0)
+        task_2 = asyncio.create_task(crash_once())
+
+        with pytest.raises(FirstCrash):
+            await task_1
+
+        assert await task_2 == 2
+
+    with time_machine.travel(t2):
+        assert await crash_once() == 2
+
+    with time_machine.travel(t3):
+        assert await crash_once() == 3
--- src/verdandi/util/cache.py
+++ src/verdandi/util/cache.py
@@ -60,28 +60,31 @@ def async_time_cache[**P, R](
                 del cache_store[key]
 
             # If a compute event has been created for this key, wait for the
-            # computation to finish and return the resulting cache key.
-            if compute_event := cache_compute_events.get(cache_key):
+            # computation to finish. We are not ensured that a result will be
+            # added to the cache store because the computation may have raised
+            # an exception.
+            while compute_event := cache_compute_events.get(cache_key):
                 await compute_event.wait()
-                return cache_store[cache_key].value
 
             # Create a new compute event (which ensures no concurent call will
             # happen)
             compute_event = Event()
             cache_compute_events[cache_key] = compute_event
 
-            # Fetch and update cache
-            cache_slot: CacheSlot | None = cache_store.get(cache_key)
+            try:
+                # Fetch and update cache
+                cache_slot: CacheSlot | None = cache_store.get(cache_key)
 
-            if cache_slot is None or cache_slot.expiration < now:
-                value = await func(*args, **kwargs)
-                cache_slot = CacheSlot(expiration=now + persistance, value=value)
-                cache_store[cache_key] = cache_slot
+                if cache_slot is None or cache_slot.expiration < now:
+                    value = await func(*args, **kwargs)
+                    cache_slot = CacheSlot(expiration=now + persistance, value=value)
+                    cache_store[cache_key] = cache_slot
 
-            # Notify the end of the event and clean it up
-            compute_event.set()
-            del cache_compute_events[cache_key]
-            return cache_slot.value
+                return cache_slot.value
+            finally:
+                # Notify the end of the event and clean it up
+                compute_event.set()
+                del cache_compute_events[cache_key]
 
         wrapper.cache_store: dict[Any, CacheSlot] = {}
         wrapper.cache_compute_events: dict[Any, Event] = {}
